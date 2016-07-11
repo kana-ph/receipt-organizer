b24aa053df50eb59fcc60e5f584ee7233d3671a2
kana0011 <kana.incognito@gmail.com>
2016-07-11 21:53:28 +0800

    Reorganize project files
    Signed-off-by: kana0011 <kana.incognito@gmail.com>

--- src/ph/kana/reor/ReceiptOrganizerJavaFx.java
+++ src/ph/kana/reor/ReceiptOrganizerJavaFx.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ph.kana.reor;
 
 import javafx.application.Application;
@@ -11,27 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author kana
- */
 public class ReceiptOrganizerJavaFx extends Application {
-	
+
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
-		
+		Parent root = FXMLLoader.load(getClass().getResource("/ph/kana/reor/fxml/Home.fxml"));
+
 		Scene scene = new Scene(root);
-		
+
 		stage.setScene(scene);
 		stage.show();
 	}
 
-	/**
-	 * @param args the command line arguments
-	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 }
---
+++ src/ph/kana/reor/controller/HomeController.java
@@ -0,0 +1,14 @@
+package ph.kana.reor.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class HomeController implements Initializable {
+
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+		// TODO
+	}
+
+}
