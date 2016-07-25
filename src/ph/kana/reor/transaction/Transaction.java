e12f31dc7e237b5ed472789e110bc6c70c33213b
kana0011 <kana.incognito@gmail.com>
2016-07-26 01:20:17 +0800

    Refactor Transaction class
    Signed-off-by: kana0011 <kana.incognito@gmail.com>

--- src/ph/kana/reor/transaction/Transaction.java
+++ src/ph/kana/reor/transaction/Transaction.java
@@ -2,30 +2,39 @@ package ph.kana.reor.transaction;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import ph.kana.reor.exception.DataAccessException;
 import ph.kana.reor.util.function.DatabaseAction;
+import ph.kana.reor.util.function.ThrowingRunnable;
 
 public final class Transaction {
 
 	private Transaction() {}
 
-	public static void execute(DatabaseAction action) throws SQLException {
+	public static void execute(DatabaseAction action) throws DataAccessException {
 		Connection connection = null;
 		try {
-			connection = start();
+			connection = ConnectionFactory.openConnection();
 			if (connection != null) {
 				action.run(connection);
 				connection.commit();
 			} else {
-				throw new SQLException("Unable to open connection");
+				throw new DataAccessException("Unable to open connection");
 			}
 		} catch (SQLException e) {
-			connection.rollback();
+			safeExecute(connection, connection::rollback);
+			throw new DataAccessException(e);
 		} finally {
-			connection.close();
+			safeExecute(connection, connection::close);
 		}
 	}
 
-	private static Connection start() throws SQLException {
-		return ConnectionFactory.openConnection();
+	private static void safeExecute(Connection connection, ThrowingRunnable<SQLException> runnable) throws DataAccessException {
+		try {
+			if (connection != null) {
+				runnable.runWithThrowable();
+			}
+		} catch (SQLException e) {
+			throw new DataAccessException(e);
+		}
 	}
 }
