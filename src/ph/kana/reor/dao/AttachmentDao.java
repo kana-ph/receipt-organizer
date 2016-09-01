7274673762079039f42cb998570a8f7b6ae092da
kana0011 <kana.incognito@gmail.com>
2016-09-02 00:02:02 +0800

    Implement saving attachment in dao layer
    Signed-off-by: kana0011 <kana.incognito@gmail.com>

--- src/ph/kana/reor/dao/AttachmentDao.java
+++ src/ph/kana/reor/dao/AttachmentDao.java
@@ -8,4 +8,6 @@ import ph.kana.reor.model.Document;
 public abstract class AttachmentDao extends AbstractDao<Attachment> {
 
 	public abstract Set<Attachment> findAllByDocument(Document document) throws DataAccessException;
+
+	public abstract Attachment save(Attachment attachment) throws DataAccessException;
 }
--- src/ph/kana/reor/dao/derby/DerbyAttachmentDao.java
+++ src/ph/kana/reor/dao/derby/DerbyAttachmentDao.java
@@ -3,6 +3,7 @@ package ph.kana.reor.dao.derby;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -26,6 +27,21 @@ public class DerbyAttachmentDao extends AttachmentDao {
 			});
 			return new HashSet(results);
 		}
+	}
+
+	@Override
+	public Attachment save(Attachment attachment) throws DataAccessException {
+		String sql = "INSERT INTO attachment(path, document_id) VALUES (?, ?)";
+
+		execute(attachment, connection -> {
+			PreparedStatement statement = connection.prepareStatement(sql, RETURN_GENERATED_KEYS);
+			statement.setString(1, attachment.getPath());
+			statement.setLong(2, attachment.getDocument().getId());
+			statement.executeUpdate();
+
+			return fetchInsertId(statement);
+		});
+		return attachment;
 	}
 
 	@Override
