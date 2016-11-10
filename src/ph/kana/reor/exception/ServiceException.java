bd9e619b8eb934d4258644d62ab18d26342017bb
kana0011 <kana.incognito@gmail.com>
2016-11-10 23:33:45 +0800

    Change ServiceException to unchecked
    Signed-off-by: kana0011 <kana.incognito@gmail.com>

--- src/ph/kana/reor/exception/ServiceException.java
+++ src/ph/kana/reor/exception/ServiceException.java
@@ -1,6 +1,6 @@
 package ph.kana.reor.exception;
 
-public class ServiceException extends Exception {
+public class ServiceException extends RuntimeException {
 
 	public ServiceException(Throwable cause) {
 		super(cause.getMessage(), cause);
